290a2fe29b0ccf43b60c1d456558112b3322d808
minalsv <57864362+minalsv@users.noreply.github.com>
2021-01-03 17:45:15 +0100

    Add files via upload
    Modified to remove the out of scope bullet.

--- bulletSystem.js
+++ bulletSystem.js
@@ -1,3 +1,7 @@
+/*
+Step 1: In bulletSystem.js complete the function edges() by looping over the bullets array and for each bullet check if it has left the screen. If it has, remove the specific bullet from the array. (Hint: Use splice() to remove the bullet from the array). Remember: If you want to call a function or use any variable that belongs to the class you'll need to prefix it with the word "this". So in this case it would be: this.bullets.myFunction();
+*/
+
 class BulletSystem {
 
   constructor(){
@@ -34,5 +38,18 @@ class BulletSystem {
   //check if bullets leave the screen and remove them from the array
   edges(){
       // YOUR CODE HERE (3 lines approx)
+	  // identify if the bullet has gone out of the screen and if so then remove it from the array.
+	  var i=0;
+	  while( i < this.bullets.length ){
+		  if( this.bullets[i].y <= 0 ){
+			  this.bullets.splice(i,1); // remove the bullet at position i and DO NOT increment the i
+			  console.log('Removed the bullet'); //Keeping it just for testing purpose.
+		  }
+		  else{
+			  i++; // incrementing i here is important since the array length would reduce by 1 if the splice gets executed.
+		  }
+			  
+	  }
+		  
   }
 }
